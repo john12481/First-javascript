b61c57223e3729d383359b2f4efb9352e239c864
john12481 <akposj423@gmail.com>
2024-09-17 12:33:33 +0100

    Add files via upload

---
+++ first js/index.js
@@ -0,0 +1,13 @@
+let name = "Favour okerri";
+let age = 14;
+let fee = 200000;
+let paid = 150000;
+let oweing = 50000;
+
+console.log(" === Welcome to my School === ");
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Fee: " + fee + "K");
+console.log("Paid: " + paid);
+console.log("Oweing: " + oweing);
+console.log("Please pay balance before on the 26th of september");
